da0fa786c35ce9966b96db072b9d5b78955c7366
aradu12 <35245025+aradu12@users.noreply.github.com>
2019-03-10 14:54:47 -0600

    Update 4a.sql
    change so that 0's are included

--- src/4a.sql
+++ src/4a.sql
@@ -1,8 +1,9 @@
 -- get the number of cessions that each author will be participating in
 -- authors only participate if their paper is accepted
 -- according to the forum, the session count is non distinct
+-- this solution includes authors with 0 acceted papers
 
-SELECT author, COUNT(csession) as "Number of Sessions"
-FROM papers
-WHERE decision = "A"
-GROUP BY author
+SELECT  p1.author, COUNT(csession) 
+FROM (SELECT DISTINCT author FROM papers) as p1
+left JOIN papers ON (papers.author = p1.author AND papers.decision = "A"  ) 
+GROUP BY p1.author
