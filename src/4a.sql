1b984a61ca0f64a26bd068483da84e61841aabee
aradu12 <35245025+aradu12@users.noreply.github.com>
2019-03-10 14:57:15 -0600

    label col

--- src/4a.sql
+++ src/4a.sql
@@ -3,7 +3,7 @@
 -- according to the forum, the session count is non distinct
 -- this solution includes authors with 0 acceted papers
 
-SELECT  p1.author, COUNT(csession) 
+SELECT  p1.author, COUNT(csession) as "Number of Sessions" 
 FROM (SELECT DISTINCT author FROM papers) as p1
 LEFT JOIN papers ON (papers.author = p1.author AND papers.decision = "A"  ) 
 GROUP BY p1.author
